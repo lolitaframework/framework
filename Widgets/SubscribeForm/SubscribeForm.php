6d68a606621b559ea42a03228ca7eeb09da0de1d
gcofficial <gurievcreative@gmail.com>
2016-06-09 17:14:16 +0300

    working on subscribe

--- Widgets/SubscribeForm/SubscribeForm.php
+++ Widgets/SubscribeForm/SubscribeForm.php
@@ -1,23 +1,22 @@
 <?php
-namespace duidluck\LolitaFramework\Widgets\SubscribeForm;
+namespace redbrook\LolitaFramework\Widgets\SubscribeForm;
 
-use \duidluck\LolitaFramework\Widgets\AbstractWithControls\AbstractWithControls as AbstractWithControls;
-use \duidluck\LolitaFramework\Core\View as View;
-use \duidluck\LolitaFramework\Core\HelperArray as HelperArray;
-use \duidluck\LolitaFramework as LolitaFramework;
+use \redbrook\LolitaFramework\Widgets\AbstractWithControls\AbstractWithControls as AbstractWithControls;
+use \redbrook\LolitaFramework\Core\View as View;
+use \redbrook\LolitaFramework\Core\HelperArray as HelperArray;
+use \redbrook\LolitaFramework\Widgets\SubscribeForm\vendor\DrewM\MailChimp\MailChimp as MailChimp;
 
-class SubscribeForm extends AbstractWithControls
-{
+class SubscribeForm extends AbstractWithControls{
     /**
      * Register widget with WordPress.
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(
             __('Lolita subscribe form', 'lolita'),
             array('description' => __('Subscribe form widget', 'lolita'))
         );
         add_action('wp_enqueue_scripts', array(&$this, 'addScriptsAndStyles'));
+        add_action('admin_enqueue_scripts', array(&$this, 'adminAddScriptsAndStyles'));
         add_action('wp_ajax_lolita_subscribe', array(&$this, 'subscribe'));
         add_action('wp_ajax_nopriv_lolita_subscribe', array(&$this, 'subscribe'));
     }
@@ -27,17 +26,56 @@ class SubscribeForm extends AbstractWithControls
      */
     public function subscribe()
     {
-        $result = wp_mail(
-            get_bloginfo('admin_email'),
-            'New subscriber',
-            'Thank you for leave us your email!'
-        );
+        $response = $_POST;
+        if (array_key_exists('type', $response) && '' !== $response['type']) {
+            $method = sprintf('subscribe%s', ucwords($response['type']));
+            if (method_exists($this, $method)) {
+                $result = $this->$method($response);
+            }
+        } else {
+            $result = false;
+        }
+        
         if (true === $result) {
             wp_send_json_success($result);
         } else {
             wp_send_json_error($result);
         }
-        
+    }
+
+    /**
+     * Subscribe default type
+     * @param  array $response request response.
+     * @return boolean result.
+     */
+    public function subscribeDefault($response)
+    {
+        return wp_mail(
+            get_bloginfo('admin_email'),
+            'New subscriber',
+            $reponse['value'] . '. Thank you for leave us your email!'
+        );
+    }
+
+    /**
+     * Subscribe mailchimp type
+     * @param  array $response request response.
+     * @return boolean result.
+     */
+    public function subscribeMailchimp($response)
+    {
+        $mail_chimp = new MailChimp($response['mailchimp_api_key']);
+        $result = $mail_chimp->post(
+            sprintf('lists/%s/members', $response['mailchimp_list_id']),
+            array(
+                'email_address' => $response['value'],
+                'status'        => 'subscribed',
+            )
+        );
+        if ('subscribed' === $result['status']) {
+            return true;
+        }
+        return $result;
     }
 
     /**
@@ -45,7 +83,7 @@ class SubscribeForm extends AbstractWithControls
      */
     public function addScriptsAndStyles()
     {
-        $assets = LolitaFramework::getURLByDirectory(__DIR__) . DS . 'assets' . DS;
+        $assets = $this->getURL() . 'assets' . DS;
         // ==============================================================
         // Scripts
         // ==============================================================
@@ -71,11 +109,27 @@ class SubscribeForm extends AbstractWithControls
         );
     }
 
+    public function adminAddScriptsAndStyles()
+    {
+        $assets = $this->getURL() . 'assets' . DS;
+        // ==============================================================
+        // Scripts
+        // ==============================================================
+        wp_enqueue_script('jquery');
+        wp_enqueue_script(
+            'lolita-widget-subscribe-form',
+            $assets . 'js' . DS . 'admin_lolita_widget_subscribe_form.js',
+            array('jquery'),
+            false,
+            true
+        );
+    }
+
     /**
      * Get controls data
      * @return array data to generate controls.
      */
-    public static function getControlsData()
+    public function getControlsData()
     {
         return array(
             array(
@@ -83,6 +137,27 @@ class SubscribeForm extends AbstractWithControls
                 '__TYPE__' => 'Input',
                 'type'     => 'text',
                 'label'    => 'Title',
+            ),
+            array(
+                'name'     => 'type',
+                '__TYPE__' => 'Select',
+                'label'    => 'Subscribe form type',
+                'options'  => array(
+                    'default'   => 'Default',
+                    'mailchimp' => 'Mailchimp',
+                )
+            ),
+            array(
+                'name'     => 'mailchimp_api_key',
+                '__TYPE__' => 'Input',
+                'type'     => 'text',
+                'label'    => 'Mailchimp API key',
+            ),
+            array(
+                'name'     => 'mailchimp_list_id',
+                '__TYPE__' => 'Input',
+                'type'     => 'text',
+                'label'    => 'Mailchimp list id',
             ),
             array(
                 'name'     => 'description',
@@ -116,8 +191,7 @@ class SubscribeForm extends AbstractWithControls
      * @param array $args     Widget arguments.
      * @param array $instance Saved values from database.
      */
-    public function widget($args, $instance)
-    {
+    public function widget( $args, $instance ) {
         $instance['success_message'] = HelperArray::get(
             $instance,
             'success_message',
@@ -125,8 +199,23 @@ class SubscribeForm extends AbstractWithControls
         );
         $instance['error_message'] = HelperArray::get(
             $instance,
-            'success_message',
+            'error_message',
             'Message not sent. Please contact the administrator for help.'
+        );
+        $instance['mailchimp_api_key'] = HelperArray::get(
+            $instance,
+            'mailchimp_api_key',
+            ''
+        );
+        $instance['mailchimp_list_id'] = HelperArray::get(
+            $instance,
+            'mailchimp_list_id',
+            ''
+        );
+        $instance['type'] = HelperArray::get(
+            $instance,
+            'type',
+            'default'
         );
         echo View::make(
             dirname(__FILE__) . DS . 'views' . DS . $this->id_base . '.php',
