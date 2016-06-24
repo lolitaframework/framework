b671c184ac68baa20ecea478f84ffe5fb482f283
gcofficial <gurievcreative@gmail.com>
2016-06-24 09:31:01 +0300

    travis

--- Widgets/AbstractWithControls/views/row.php
+++ Widgets/AbstractWithControls/views/row.php
@@ -1,8 +1,8 @@
 <div id="<?php echo $control->getID(); ?>_row" class="widget_control_row">
-	<p>
-		<label for="<?php echo $control->getID(); ?>">
-		    <?php echo $control->parameters['label']; ?>:
-		</label>
-	</p>
+    <p>
+        <label for="<?php echo $control->getID(); ?>">
+            <?php echo $control->parameters['label']; ?>:
+        </label>
+    </p>
     <?php echo $control->render(); ?>
 </div>
