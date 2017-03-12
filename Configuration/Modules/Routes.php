6abdaf86f8577f90fd3ca90f1e2c4a504f24f29b
Александр Кучерявый <alexandr.kucheriavyi@gmail.com>
2017-03-13 00:12:49 +0200

    Added the ability to create routs with arguments

--- Configuration/Modules/Routes.php
+++ Configuration/Modules/Routes.php
@@ -93,6 +93,18 @@ class Routes implements IModule
      */
     private function render($element)
     {
+        if (is_array($element) && array_key_exists('method', $element)
+            && array_key_exists('args', $element)) {
+
+            if (is_callable($element['method'])) {
+                $method = $element['method'];
+                $args = $element['args'];
+                return forward_static_call_array($method, array($args));
+            } else {
+                $element = $element['method'];
+            }
+        }
+
         if (is_callable($element)) {
             return $element();
         }
@@ -156,16 +168,131 @@ class Routes implements IModule
 
         $tmpl = get_page_template_slug(get_queried_object_id());
 
-        if ($page && array_key_exists($page, $this->data)) {
-            status_header(200);
-            echo $this->render($this->data[ $page ]);
-            exit;
+        if ($page) {
+            if (array_key_exists($page, $this->data)) {
+                status_header(200);
+                echo $this->render($this->data[$page]);
+                exit;
+            }
+
+            $data = $this->searchMatchingWithMask($page);
+
+            if ($data) {
+                status_header(200);
+                echo $this->render($data);
+                exit;
+            }
         }
+
         if ($tmpl && array_key_exists($tmpl, $this->data)) {
             status_header(200);
             echo $this->render($this->data[ $tmpl ]);
             exit;
         }
+    }
+
+    /**
+     * Search matches in routs with arguments by mask
+     *
+     * @param $page
+     * @return array
+     */
+    private function searchMatchingWithMask($page)
+    {
+        $data = array();
+
+        foreach ($this->data as $rout => $method) {
+            $rout = trim($rout, '/');
+
+            if (!$this->isRouteWithArguments($rout)) {
+                continue;
+            }
+
+            $args_names = $this->getRouteArgsNames($rout);
+
+            if (!$args_names) {
+                continue;
+            }
+
+            $args_values = $this->getRouteArgsValues($rout, $page);
+
+            if (!$args_values) {
+                continue;
+            }
+
+            if (count($args_names) != count($args_values)) {
+                continue;
+            }
+
+            $data['method'] = $method;
+            $data['args'] = $this->prepareRoutsArgs($args_names, $args_values);
+            break;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Check if rout has mask of arguments
+     *
+     * @param $rout
+     * @return bool
+     */
+    private function isRouteWithArguments($rout)
+    {
+        return preg_match("/%.*%/", $rout) != false;
+    }
+
+    /**
+     * Get routs arguments names
+     *
+     * @param $rout
+     * @return array
+     */
+    private function getRouteArgsNames($rout)
+    {
+        preg_match_all("/%(.*)%/U", $rout, $matches);
+
+        return (isset($matches[1])) ? $matches[1] : array();
+    }
+
+    /**
+     * Get routs arguments values
+     *
+     * @param $rout
+     * @param $page
+     * @return array
+     */
+    private function getRouteArgsValues($rout, $page)
+    {
+        $mask = str_replace('/', '\/', $rout);
+        $mask = preg_replace('/(%.*%)/U', '([^\/]*)', $mask);
+        $mask .= '$';
+
+        preg_match_all('/'.$mask.'/', $page, $matches);
+
+        array_shift($matches);
+
+        return (!isset($matches) || !$matches[0]) ? array() : $matches;
+    }
+
+    /**
+     * Prepare routs arguments array
+     *
+     * @param $args_names
+     * @param $args_values
+     * @return array
+     */
+    private function prepareRoutsArgs($args_names, $args_values)
+    {
+        $args = array();
+
+        foreach ($args_names as $num => $name) {
+            $fist_element_ket = 0;
+            $args[$name] = $args_values[$num][$fist_element_ket];
+        }
+
+        return $args;
     }
 
     /**
