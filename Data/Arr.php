edf51ff916f71df3d9c1265ded7fc6128070b3fa
gcofficial <gurievcreative@gmail.com>
2019-05-31 17:16:27 +0300

    core: compact

--- Data/Arr.php
+++ Data/Arr.php
@@ -7,19 +7,37 @@ namespace LolitaFramework\Data;
  */
 class Arr {
 
-  /**
-   * Append item to array
-   *
-   * @usage lf::append([1, 2, 3], 4);
-   *        >> [1, 2, 3, 4]
-   *
-   * @param array $array original array.
-   * @param mixed $value new item or value to append.
-   *
-   * @return array
-   */
-  public static function append( $array = [], $value = null ) {
-      $array[] = $value;
-      return $array;
-  }
+	/**
+	* Append item to array
+	*
+	* @usage lf::append([1, 2, 3], 4);
+	*        >> [1, 2, 3, 4]
+	*
+	* @param array $array original array.
+	* @param mixed $value new item or value to append.
+	*
+	* @return array
+	*/
+	public static function append( $array = [], $value = null ) {
+		$array[] = $value;
+		return $array;
+	}
+
+	/**
+	 * Creates  an  array  with  all  falsey  values removed. The values false, null, 0, "", undefined, and NaN are all
+	 * falsey.
+	 *
+	 * @usage __::compact([0, 1, false, 2, '', 3]);
+	 *        >> [1, 2, 3]
+	 *
+	 * @param array $array array to compact
+	 *
+	 * @return array
+	 */
+	public static function compact($array = [])
+	{
+		return array_values(
+			array_filter($array, function($var) { return $var; })
+		);
+	}
 }
--- tests/ArrTest.php
+++ tests/ArrTest.php
@@ -9,13 +9,23 @@ use PHPUnit\Framework\TestCase;
  */
 class ArrTest extends TestCase {
 
-    /**
-     * Test append function
-     */
-    public function testAppend() {
-        $this->assertEquals(
-            [ 1, 2, 3, 4 ],
-            LF::append( [ 1, 2, 3 ], 4 )
-        );
-    }
+	/**
+	 * Test append function
+	 */
+	public function testAppend() {
+		$this->assertEquals(
+			[ 1, 2, 3, 4 ],
+			LF::append( [ 1, 2, 3 ], 4 )
+		);
+	}
+
+	/**
+	 * Test compact function
+	 */
+	public function testCompact() {
+		$this->assertEquals(
+			[1, 2, 3],
+			LF::compact([0, 1, false, 2, '', 3])
+		);
+	}
 }
