71c463b58f4a1ca6287f5a9e1008f836c007d676
gcofficial <gurievcreative@gmail.com>
2019-06-02 15:25:30 +0300

    core: get

--- Data/Arr.php
+++ Data/Arr.php
@@ -111,6 +111,21 @@ class Arr {
 	}
 
 	/**
+	 * Determine if the given key exists in the provided array.
+	 *
+	 * @param  \ArrayAccess|array  $array
+	 * @param  string|int  $key
+	 * @return bool
+	 */
+	public static function exists( $array, $key ) {
+		if ( $array instanceof ArrayAccess ) {
+			return $array->offsetExists( $key );
+		}
+
+		return array_key_exists( $key, $array );
+	}
+
+	/**
 	 * Get an item from an array using "dot" notation.
 	 *
 	 * @param  \ArrayAccess|array  $array
@@ -118,23 +133,22 @@ class Arr {
 	 * @param  mixed   $default
 	 * @return mixed
 	 */
-	public static function get($array, $key, $default = null)
-	{
-		if (! static::accessible($array)) {
+	public static function get( $array, $key, $default = null ) {
+		if ( ! static::accessible( $array ) ) {
 			return $default;
 		}
 
-		if (is_null($key)) {
+		if ( is_null( $key ) ) {
 			return $array;
 		}
 
-		if (static::exists($array, $key)) {
+		if ( static::exists( $array, $key ) ) {
 			return $array[$key];
 		}
 
-		foreach (explode('.', $key) as $segment) {
-			if (static::accessible($array) && static::exists($array, $segment)) {
-				$array = $array[$segment];
+		foreach ( explode( '.', $key ) as $segment ) {
+			if ( static::accessible( $array ) && static::exists( $array, $segment ) ) {
+				$array = $array[ $segment ];
 			} else {
 				return $default;
 			}
